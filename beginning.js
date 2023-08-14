594bb11a5a80d53cf8bfaefeb5cd458075a81278
Premosh <138636889+Premosh@users.noreply.github.com>
2023-08-14 10:57:00 +0545

    Add files via upload

---
+++ beginning.js
@@ -0,0 +1,12 @@
+function printHello(){
+    document.getElementById("demo").innerHTML="Hello";
+    window.alert('Fuck you');
+    console.log('Hello');
+}
+function printHI(){
+    const pi=3.14;
+    let a=1;
+    let b=2;
+    let c=(15/100)*5000;
+    document.getElementById("demo1").innerHTML=c;
+}
