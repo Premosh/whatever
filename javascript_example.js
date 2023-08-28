e82484b10b0ba8f582dd7de8cf32a5ce0ec4f5be
Premosh <138636889+Premosh@users.noreply.github.com>
2023-08-27 22:20:41 -0700

    Update javascript_example.js

--- javascript_example.js
+++ javascript_example.js
@@ -5,7 +5,17 @@ function onSubmit(event){
     var name=document.getElementById('name').value;
     var email=document.getElementById('email').value;
     var gender=document.querySelector('input[name="gender"]:checked').value;
-    document.getElementById('formData').innerHTML="Name: "+name+"<br>Email: "+email+"<br>Gender:"+gender;
+    var acceptLicenseYes = document.getElementById('acceptLicenseYes');
+    var acceptLicenseNo = document.getElementById('acceptLicenseNo');
+    var acceptLicense;
+    if (acceptLicenseYes.checked==true && acceptLicenseNo.checked==true) {
+        acceptLicense = "cannot select two checkboxes";
+    } else if (acceptLicenseYes.checked=true) {
+        acceptLicense = document.getElementById('acceptLicenseYes').value;
+    } else if (acceptLicenseNo.checked=true) {
+        acceptLicense = document.getElementById('acceptLicenseNo').value;
+    }
+    document.getElementById('formData').innerHTML = "Name:" + name + "<br>Email:" + email + "<br>Gender:" + gender + "<br>Accept license:" + acceptLicense;
 }
 window.onload=function(){
     window.alert('This page is not secure');
